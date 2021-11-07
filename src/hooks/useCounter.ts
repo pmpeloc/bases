fc7f80ff01966c345f20a1b7048aea75614e42c8
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-07 02:01:41 -0300

    Custom Hooks con referencia HTML

---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
+
+export const useCounter = () => {
+  const MAXIMUN_COUNT = 10;
+
+  const [counter, setCounter] = useState(8);
+
+  const counterElement = useRef<HTMLHeadingElement>(null);
+
+  const handleClick = () => {
+    setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
+  };
+
+  useEffect(() => {
+    if (counter < 10) return;
+    console.log(
+      '%cSe llegó al valor máximo',
+      'color: red; background-color: black;'
+    );
+    const tl = gsap.timeline();
+    tl.to(counterElement.current, {
+      y: -10,
+      duration: 0.2,
+      ease: 'ease.out',
+    }).to(counterElement.current, {
+      y: 0,
+      duration: 1,
+      ease: 'bounce.out',
+    });
+  }, [counter]);
+  return {
+    counter,
+    counterElement,
+    handleClick,
+  };
+};
