1ca220bd5ea87f9a84a788ee4fe95757de3d045c
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-07 00:56:10 -0300

    Trabajando con PropTypes

--- src/bases/Counter.tsx
+++ src/bases/Counter.tsx
@@ -1,17 +1,20 @@
-import { useState} from 'react'
+import { useState } from 'react';
 
-export const Counter = ({initialValue = 0}) => {
+interface Props {
+  initialValue?: number;
+}
 
+export const Counter = ({ initialValue = 0 }: Props) => {
   const [counter, setCounter] = useState(initialValue);
 
   const handleClick = () => {
-    setCounter(prev => prev + 1);
-  }
+    setCounter((prev) => prev + 1);
+  };
 
   return (
     <>
       <h1>Counter: {counter}</h1>
-      <button onClick={handleClick} >+1</button>
+      <button onClick={handleClick}>+1</button>
     </>
-  )
-}
+  );
+};
