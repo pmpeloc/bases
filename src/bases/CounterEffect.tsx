7fa0019a5948329476143bd3b4fe1f9ded993735
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-07 01:35:57 -0300

    Realizar animación cuando se llega al valor máximo

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
 
@@ -15,11 +16,15 @@ export const CounterEffect = () => {
       '%cSe llegó al valor máximo',
       'color: red; background-color: black;'
     );
+    gsap.to('h2', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() => {
+      gsap.to('h2', { y: 0, duration: 1, ease: 'bounce.out' });
+    });
   }, [counter]);
 
   return (
     <>
-      <h1>CounterEffect: {counter}</h1>
+      <h1>CounterEffect:</h1>
+      <h2>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </>
   );
