d8d7757a190605e938512b53ffd1612f3cf0019a
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-09 12:42:25 -0300

    Action Creators

--- src/counter-reducer/CounterReducer.tsx
+++ src/counter-reducer/CounterReducer.tsx
@@ -1,4 +1,5 @@
 import { useReducer } from 'react';
+import * as actions from './actions/actions';
 import { CounterState } from './interfaces/interfaces';
 import { counterReducer } from './state/counterReducer';
 
@@ -13,11 +14,11 @@ export const CounterReducerComponent = () => {
   const [counterState, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
   const handleReset = () => {
-    dispatch({ type: 'reset' });
+    dispatch(actions.doReset());
   };
 
   const increaseBy = (value: number) => {
-    dispatch({ type: 'increaseBy', payload: { value } });
+    dispatch(actions.doIncreaseBy(value));
   };
 
   return (
